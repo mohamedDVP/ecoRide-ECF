5ad99249ae2f4ab9131a9747032f4c43febd17bb
mohamedDVP <mohamed.mak@laposte.net>
2025-05-15 19:50:06 +0200

    entity Voiture without relations

---
+++ src/Entity/Voiture.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoitureRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoitureRepository::class)]
+class Voiture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $modele = null;
+
+    #[ORM\Column(length: 10, nullable: true)]
+    private ?string $immatriculation = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $energie = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $couleur = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTime $dateImmatriculation = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getModele(): ?string
+    {
+        return $this->modele;
+    }
+
+    public function setModele(?string $modele): static
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+
+    public function getImmatriculation(): ?string
+    {
+        return $this->immatriculation;
+    }
+
+    public function setImmatriculation(?string $immatriculation): static
+    {
+        $this->immatriculation = $immatriculation;
+
+        return $this;
+    }
+
+    public function getEnergie(): ?string
+    {
+        return $this->energie;
+    }
+
+    public function setEnergie(?string $energie): static
+    {
+        $this->energie = $energie;
+
+        return $this;
+    }
+
+    public function getCouleur(): ?string
+    {
+        return $this->couleur;
+    }
+
+    public function setCouleur(?string $couleur): static
+    {
+        $this->couleur = $couleur;
+
+        return $this;
+    }
+
+    public function getDateImmatriculation(): ?\DateTime
+    {
+        return $this->dateImmatriculation;
+    }
+
+    public function setDateImmatriculation(?\DateTime $dateImmatriculation): static
+    {
+        $this->dateImmatriculation = $dateImmatriculation;
+
+        return $this;
+    }
+}
